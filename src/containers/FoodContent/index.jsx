95914f0b0a0cf52fd1cc9d0c649f5b6b50837237
sunxx <2659528347@qq.com>
2017-06-02 01:09:16 +0800

    feat: foodcontent

--- src/containers/FoodContent/index.jsx
+++ src/containers/FoodContent/index.jsx
@@ -8,14 +8,16 @@ import './index.scss'
 const propTypes = {
     fetchFoodContent: PropTypes.func.isRequired,
     id: PropTypes.number.isRequired,
-    content: PropTypes.string.isRequired,
+    content: PropTypes.array.isRequired,
     name: PropTypes.string.isRequired,
+    imagePath: PropTypes.string.isRequired,
 }
 
 const defaultProps = {
     id: 0,
-    content: '',
+    content: [],
     name: '',
+    imagePath: '',
 }
 
 class FoodContent extends Component {
@@ -36,13 +38,84 @@ class FoodContent extends Component {
     }
 
     render() {
-        const { content, } = this.props
+        const { content, imagePath, } = this.props
         const { name, } = this.props
         changeTitle(name)
+        const {
+            contentA,
+            contentB,
+            contentC,
+            contentD,
+            stageA,
+            stageB,
+            stageC,
+            stageD,
+            alias_,
+            tips,
+        } = content[0] || []
         return (
-            <div
-                className="FoodContent-container"
-            />
+            <div className="foodContent-container">
+                <div className="image">
+                    <img src={imagePath} alt="食物图片" />
+                </div>
+                <div className="title">
+                    <p>{alias_}</p>
+                </div>
+                <div className="content">
+                    <div className="item">
+                        <h1>孕妇能吃吗？</h1>
+                        <div className="item-content">
+                            <div className="cannotEat-box">
+                                {stageA === 1 ?
+                                    <div className="status-icon1">能吃</div> :
+                                    <div className="status-icon2">不能吃</div>
+                                }
+                            </div>
+                            <p>{contentA}</p>
+                        </div>
+                    </div>
+                    <div className="item">
+                        <h1>坐月子能吃吗？</h1>
+                        <div className="item-content">
+                            <div className="cannotEat-box">
+                                {stageB === 1 ?
+                                    <div className="status-icon1">能吃</div> :
+                                    <div className="status-icon2">不能吃</div>
+                                }
+                            </div>
+                            <p>{contentB}</p>
+                        </div>
+                    </div>
+                    <div className="item">
+                        <h1>哺乳能吃吗？</h1>
+                        <div className="item-content">
+                            <div className="cannotEat-box">
+                                {stageC === 1 ?
+                                    <div className="status-icon1">能吃</div> :
+                                    <div className="status-icon2">不能吃</div>
+                                }
+                            </div>
+                            <p>{contentC}</p>
+                        </div>
+                    </div>
+                    <div className="item">
+                        <h1>婴儿能吃吗？</h1>
+                        <div className="item-content">
+                            <div className="cannotEat-box">
+                                {stageD === 1 ?
+                                    <div className="status-icon1">能吃</div> :
+                                    <div className="status-icon2">不能吃</div>
+                                }
+                            </div>
+                            <p>{contentD}</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="tips">
+                    <h1>小贴士</h1>
+                    <p>{tips}</p>
+                </div>
+            </div>
         )
     }
 }
@@ -53,10 +126,11 @@ FoodContent.defaultProps = defaultProps
 
 const mapStateToProps = ({ ...state }, { location, }) => {
     const { id, } = location.query
-    const { vaccineTimeTable: { content, name, }, } = state
+    const { foodList: { content, name, imagePath, }, } = state
     return {
         content,
         name,
+        imagePath,
         id: Number(id),
     }
 }
